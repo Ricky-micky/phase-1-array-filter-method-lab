814ba956267f20a1a0d0a07b45bb1fa82cce529b
Ricky-micky <erick.tembo@student.moringaschool.com>
2024-10-06 17:06:51 +0300

    added text

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+    function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  const drivers = ["mark", "steve", "rose", "lion", "Alfred"];
+  const driverObjects = [
+    { name: "mark", hometown: "meru" },
+    { name: "steve", hometown: "malindi" },
+    { name: "rose", hometown: "timboni" }
+  ];
+  
+  console.log(findMatching(drivers, "lion"));
+  console.log(fuzzyMatch(drivers, "malindi")); 
+console.log(matchName(driverObjects, "lion"));
+  
 // Code your solution here
